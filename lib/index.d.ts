40672a082d9272e85008eb1c45c6b01ae919db9b
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-01 01:37:55 +0900

    publish 2.4.1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,10 @@
+export { ArrayUtils } from './array/ArrayUtils';
 export { CustomError } from './error/CustomError';
-export { Request } from './http/Request';
-export type { RequestGetResult, RequestPostResult } from './http/Request';
+export { EnvParser } from './paser/Paser';
 export { JSTDate } from './date/JSTDate';
 export { Logger } from './logger/Logger';
 export type { LoggerOptions } from './logger/Logger';
 export { ObjectUtils } from './object/ObjectUtils';
-export { EnvParser } from './paser/Paser';
-export { ArrayUtils } from './array/ArrayUtils';
+export { Request } from './http/Request';
+export type { RequestGetResult, RequestPostResult } from './http/Request';
 export { StringUtils } from './string/StringUtils';
